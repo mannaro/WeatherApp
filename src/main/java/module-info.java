2859aafdace5f52b0ba2fc7d15a404bb883deb93
Mannaro <max_ximus@mail.ru>
2024-02-22 22:36:51 +0500

    init repo

---
+++ src/main/java/module-info.java
@@ -0,0 +1,14 @@
+module ru.mannaro.WeatherApp {
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires org.json;
+
+    opens ru.mannaro.weatherapp to javafx.fxml;
+    exports ru.mannaro.weatherapp;
+}
